25c0b3c81607af3dca651e8399f2a6a649dfa879
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2020-04-15 20:32:04 +0300

    finishing beclean

--- BeClean.go
+++ BeClean.go
@@ -6,28 +6,28 @@ func main () {
   // solve https://thecode.media/ill-be-clean/
   // i didn't look at solution, done all myself
   var perimetr  int
-  var height, square float
+  var height, square float32
 
   height = 2.5
   perimetr = 50
-  square = height * float(perimetr)
+  square = height * float32(perimetr)
 
   var sanSquare int
   sanSquare = 5
 
-  var sanNeeded float
-  sanNeeded = square / sanSquare
+  var sanNeeded float32
+  sanNeeded = square / float32(sanSquare)
 
-  var famSize, sanPerDay, sanAvailable, cleanReg int
+  var famSize, sanPerDay, cleanReg int
+  var sanAvailable float32
   famSize = 3
   sanPerDay = 4
   cleanReg = 2
-  sanAvailable = famSize * sanPerDay * cleanReg
+  sanAvailable = float32(famSize * sanPerDay * cleanReg)
 
   if sanNeeded < sanAvailable {
       fmt.Println("House can be cleaned")
-    }
-    else {
+    } else {
       fmt.Println("House can't be cleaned")
     }
 }
