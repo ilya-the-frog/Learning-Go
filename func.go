b563c1112709c00e1312ad5da925bff78b1f365e
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2020-04-17 23:17:54 +0300

    enjoying funcs

---
+++ func.go
@@ -0,0 +1,27 @@
+package main
+import (
+  "fmt"
+  "time"
+)
+
+func joinTwoStrings(prefix string, suffix string) string {
+  return prefix + suffix
+}
+
+func printOutDate() {
+  t := time.Now()
+  fmt.Println(t)
+}
+
+func waitForIt(message string) {
+  defer fmt.Println("Done!")
+  fmt.Println("Waiting")
+  fmt.Println("Waiting")
+  fmt.Println("Waiting")
+  fmt.Println(message)
+}
+
+func main() {
+  printOutDate()
+  waitForIt(joinTwoStrings("Hi", " there"))
+}
