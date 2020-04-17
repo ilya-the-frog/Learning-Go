07a74045c2f28945a86455ca190e487aa25e6752
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2020-04-18 01:19:47 +0300

    interstellar travel

---
+++ travel.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+func fuelGauge(fuel int) {
+  fmt.Println("You still have", fuel, "gallons")
+}
+
+// Create the function calculateFuel() here
+func calculateFuel(planet string) int {
+  var fuel int
+  switch planet {
+  case "Venus":
+    fuel = 300000
+  case "Mercury":
+    fuel = 500000
+  case "Mars":
+    fuel = 700000
+  default:
+    fmt.Println("We can't go here, no fuel used")
+    fuel = 0
+  }
+  return fuel
+}
+
+// Create the function greetPlanet() here
+func greetPlanet(planet string) {
+  fmt.Println("Welcome to planet", planet)
+}
+
+// Create the function cantFly() here
+func cantFly() {
+  fmt.Println("We do not have the available fuel to fly.")
+}
+
+// Create the function flyToPlanet() here
+func flyToPlanet(planet string, fuel int) int {
+  var fuelRemaining, fuelCost int
+  fuelRemaining = fuel
+  fuelCost = calculateFuel(planet)
+
+  if fuelRemaining >= fuelCost {
+    greetPlanet(planet)
+    fuelRemaining = fuelRemaining - fuelCost
+  } else {
+    cantFly()
+  }
+  return fuelRemaining
+}
+
+func dest() {
+  fmt.Println("Select destination")
+}
+
+func checkFuel(fuel int) int {
+  if fuel >= 700000 {
+    fmt.Println("We can go to Venus, Mercury or Mars")
+    dest()
+  } else if fuel >= 500000 {
+    fmt.Println("We can go to Venus or Mercury")
+    dest()
+  } else if fuel >= 300000 {
+    fmt.Println("We can go to Venus")
+    dest()
+  } else {
+    cantFly()
+  }
+  return fuel
+}
+
+func main() {
+  // Create `planetChoice` and `fuel`
+  fmt.Println("Hello Space Traveller! How much fuel you have?")
+  var fuel int
+  fmt.Scan(&fuel)
+  checkFuel(fuel)
+
+  var planetChoice string
+  fmt.Scan(&planetChoice)
+  for fuel > 300000 {
+    fuel = flyToPlanet(planetChoice, fuel)
+    checkFuel(fuel)
+    fuelGauge(fuel)
+  }
+    cantFly()
+
+}
