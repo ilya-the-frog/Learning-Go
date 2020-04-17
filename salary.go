2a2c6a4735d973d244ada8e1618448f32770aafa
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2020-04-18 00:49:22 +0300

    fix a bit

--- salary.go
+++ salary.go
@@ -8,6 +8,7 @@ func main () {
   fmt.Scan(&salary)
   netto := float32(salary) * 0.87
 
-  fmt.Printf("Your salary netto is %.2f", netto)
   fmt.Println("")
+  fmt.Printf("Your salary netto is %.2f", netto)
+
 }
