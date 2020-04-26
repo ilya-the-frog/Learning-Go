d6f4ea314fd12b875b9c2866781d57d860206e34
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2020-04-26 05:33:23 +0300

    structs-and-interfaces

---
+++ variablesTypes.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var congrats string
+  congrats = "Congratulations"
+  congrats += "!!!"
+  fmt.Println(congrats)
+
+  var challenge string = "What else can you do?"
+  fmt.Println(challenge)
+
+  reminder := "Pratice is important!"
+  fmt.Println(reminder)
+}
