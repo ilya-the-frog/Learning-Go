2f93927d2d44b5b60502e721ed61c8171554e994
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2020-05-17 00:53:34 +0300

    add link to kata

--- triangle.go
+++ triangle.go
@@ -1,14 +1,15 @@
 package kata
 
 //is that a triangle kata from codewars
+//https://www.codewars.com/kata/56606694ec01347ce800001b
 
 func IsTriangle(a, b, c int) bool {
-  if a + b > c {
-    if a + c > b {
-      if b + c > a {
-        return true
-       }
-    }
-  }
-  return false
+	if a+b > c {
+		if a+c > b {
+			if b+c > a {
+				return true
+			}
+		}
+	}
+	return false
 }
