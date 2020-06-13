6510a51596d0f1afea39d281281fd74641f01503
Shantaramka <kontroler.im@gmail.com>
2020-06-13 20:49:51 +0300

    add 1 more solution

--- sorts.go
+++ sorts.go
@@ -21,7 +21,6 @@ func main() {
 func bubleSort(s []int) []int {
 	for n := 1; n < len(s); n++ {
 		for i := 0; i < len(s)-1; i++ {
-
 			if s[i] > s[i+1] {
 				//fmt.Println("Swapping", s[i], "&", s[i+1])
 				s[i+1], s[i] = s[i], s[i+1]
