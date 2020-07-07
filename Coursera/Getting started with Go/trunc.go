04c90ae2e45324599f63068c7c139d49e7a1485a
Shantaramka <56070863+Shantaramka@users.noreply.github.com>
2020-07-07 15:46:56 +0300

    Update trunc.go

--- Coursera/Getting started with Go/trunc.go
+++ Coursera/Getting started with Go/trunc.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+/*
+Write a program which prompts the user to enter a floating point number and 
+prints the integer which is a truncated version of the floating point number 
+that was entered. Truncation is the process of removing the digits to the 
+right of the decimal place
+*/
+
 func main() {
 	var floatInput int
 	fmt.Println("Type float number")
